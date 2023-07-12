da90bf105ec215a29e30099f9545325e0275f90b
HamzaOztrk <oztrkhamza1@gmail.com>
2023-07-12 23:50:45 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [hex, setHex] = useState("#ffffff");
+
+  const randomHex = () => {
+    const randomhex = "#" + Math.floor(Math.random() * 16777721).toString(16);
+    setHex(randomhex);
+  };
+
+  return (<div className="App" style={{backgroundColor:`${hex}`}}>
+    <h1>
+      {hex}
+    </h1>
+
+    <button onClick={randomHex}>
+      click for new background
+    </button>
+
+    <button onClick={()=> navigator.clipboard.writeText(hex)}>
+      copy the hex value
+    </button>
+  </div>)
 }
 
 export default App;
